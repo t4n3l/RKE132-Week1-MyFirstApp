17735377057b0f2c2b24caeb5821d60d715d23f8
Tanel Ristsalu <tanel.ristsalu@tktk.ee>
2024-02-05 20:31:40 +0200

    my first app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Enter your name:");
+string nimi;
+nimi = Console.ReadLine();
+Console.WriteLine("Hello, " + nimi);
+Console.WriteLine($"Hello, {nimi}!");
